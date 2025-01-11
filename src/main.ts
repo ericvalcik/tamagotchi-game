6638770c139b5a8273dc5b1ac352f4db1557fa90
eric valcik <eric.valcik@gmail.com>
2025-01-11 19:43:11 +0100

    have ador there but with transparent bg

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { Application, Assets, AnimatedSprite, Sprite } from "pixi.js";
+import { Application, Assets, AnimatedSprite } from "pixi.js";
 
 (async () => {
   // Create a new application
   const app = new Application();
 
   // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
+  await app.init({ backgroundAlpha: 0, width: 300, height: 300 });
 
   // Append the application canvas to the document body
   document.getElementById("pixi-container")!.appendChild(app.canvas);
@@ -14,41 +14,21 @@ import { Application, Assets, AnimatedSprite, Sprite } from "pixi.js";
   const sheet = await Assets.load("/assets/ador.json");
 
   const adorIdle = new AnimatedSprite(sheet.animations["idle"]);
-  adorIdle.texture.baseTexture.scaleMode = "nearest";
+  adorIdle.texture.source.scaleMode = "nearest";
   adorIdle.animationSpeed = 0.07;
-  adorIdle.scale.set(16);
+  adorIdle.scale.set(4);
   adorIdle.anchor.set(0.5);
   adorIdle.position.set(app.screen.width / 2, app.screen.height / 2);
 
   const adorRun = new AnimatedSprite(sheet.animations["run"]);
-  adorRun.texture.baseTexture.scaleMode = "nearest";
+  adorRun.texture.source.scaleMode = "nearest";
   adorRun.animationSpeed = 0.12;
-  adorRun.scale.set(16);
+  adorRun.scale.set(4);
   adorRun.anchor.set(0.5);
   adorRun.position.set(app.screen.width / 2, app.screen.height / 2);
 
-  const buttonSheet = await Assets.load("/assets/start-btn.json");
-
-  const btnUp = new Sprite(buttonSheet.textures["up"]);
-  btnUp.texture.baseTexture.scaleMode = "nearest";
-  btnUp.scale.set(8);
-  btnUp.anchor.set(0.5);
-  btnUp.position.set(app.screen.width / 2, app.screen.height * 0.8);
-  btnUp.eventMode = "static";
-  btnUp.cursor = "pointer";
-  btnUp.on("pointerdown", (event) => {
-    btnUp.texture = buttonSheet.textures["down"];
-  });
-  btnUp.on("pointerup", (event) => {
-    btnUp.texture = buttonSheet.textures["up"];
-  });
-  btnUp.on("pointerout", (event) => {
-    btnUp.texture = buttonSheet.textures["up"];
-  });
-
   // Add the bunny to the stage
   app.stage.addChild(adorIdle);
-  app.stage.addChild(btnUp);
 
   // Play the animation
   adorIdle.play();
